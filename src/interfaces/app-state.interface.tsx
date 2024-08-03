41edf75f4f68e1fdead184dacb8b6dcbbfbc4397
SJSJC <flyflyinu@gmail.com>
2024-08-03 13:51:32 +0900

    login tsx and css

--- src/interfaces/app-state.interface.tsx
+++ src/interfaces/app-state.interface.tsx
@@ -1,3 +1,4 @@
 export default interface AppState {
   isLoggedIn: boolean;
+  username: string;
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,45 @@
+import "../components/styles/Login.css";
+import { useState } from "react";
 import { useAppContext, useAppDispatchContext } from "../AppContext";
 
 const Login = () => {
   const useAppState = useAppContext();
   const setAppState = useAppDispatchContext();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    setAppState({ ...useAppState, isLoggedIn: true, username });
+  };
   return (
     <section className="login">
-      Login
-      <button onClick={() => setAppState({ ...useAppState, isLoggedIn: true })}>
-        Click Me
+      <h1>Login</h1>
+      <div className="login-inputs">
+      <label>
+      Username:
+      <input
+        type = "text"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+        placeholder="Enter your username"
+      />
+      </label>
+      <label>
+      Password:
+      <input
+        type = "password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+        placeholder="Enter your password"
+      />
+      </label>
+      </div>
+      <button onClick={handleLogin}>
+        Login
       </button>
     </section>
   );
-};
 
+};
 export default Login;
