5dff98bf0c6be8859556be880c8d04fb66585af9
susheepls <169642127+susheepls@users.noreply.github.com>
2024-08-04 14:02:27 +0900

    Fix: add buttons to choose card category when creating card

--- src/components/CreateCardWindow.tsx
+++ src/components/CreateCardWindow.tsx
@@ -1,6 +1,6 @@
 import CreateCardType from "../interfaces/CreateCardType"
 import * as cardApi from "../api/card";
-import { useRef } from "react";
+import { MouseEventHandler, useRef, useState } from "react";
 
 interface Props {
     card: CreateCardType
@@ -10,24 +10,33 @@ const CreateCardWindow:React.FC<Props> = (props) => {
     const frontTextRef = useRef<HTMLTextAreaElement>(null);
     const backTextRef = useRef<HTMLTextAreaElement>(null);
 
+    const [chosenCategory, setChosenCategory] = useState<number>(3);
+
     const CreateCardOnClick = async () => {
         if(!backTextRef.current || !frontTextRef.current) return;
         const card: CreateCardType = {
             back_text: backTextRef.current.value,
             front_text: frontTextRef.current.value,
-            category_id: 3,
+            category_id: chosenCategory,
             user_id: props.card.user_id,
         };
         const response = await cardApi.createCard(card);
         frontTextRef.current.value = "";
         backTextRef.current.value = "";
     }
-
+    const handleChosenCategory = (num:number) => {
+        setChosenCategory(num);
+    }
 
     return <>
         <div className="Card-Creation-Window">
             <textarea ref={frontTextRef}></textarea>
             <textarea ref={backTextRef}></textarea>
+            <div className="Category-Choices">
+                <button onClick={() => handleChosenCategory(1)}>Joke</button>
+                <button onClick={() => handleChosenCategory(2)}>Trivia</button>
+                <button onClick={() => handleChosenCategory(3)}>Whatever</button>
+            </div>
             <button onClick={CreateCardOnClick}>submit</button>
             <button >cancel</button>
         </div>
