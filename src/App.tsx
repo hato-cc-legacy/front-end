695ee750b8b3d00de8fd320d9cec894b696479c6
Eiichi Sayama <eiichimouse@outlook.jp>
2024-08-07 20:03:03 +0900

    Add admin component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
 import "./App.css";
 
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import AppProvider from "./AppContext";
-// import Layout from "./components/Layout";
-// import Index from "./pages/Index";
-// import Login from "./pages/Login";
-// import UserPage from "./pages/UserPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AppProvider from "./AppContext";
+import Layout from "./components/Layout";
+import Index from "./pages/Index";
+import Login from "./pages/Login";
+import UserPage from "./pages/UserPage";
 import Admin from "./pages/Admin";
 
 const App = () => {
   return (
-    <Admin />
-    // <AppProvider>
-    //   <BrowserRouter>
-    //     <Routes>
-    //       <Route path="/" element={<Layout></Layout>}>
-    //         <Route index element={<Index></Index>}></Route>
-    //         <Route path="/login" element={<Login></Login>}></Route>
-    //         <Route path="/user" element={<UserPage></UserPage>}></Route>
-    //       </Route>
-    //     </Routes>
-    //   </BrowserRouter>
-    // </AppProvider>
+    // <Admin />
+    <AppProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout></Layout>}>
+            <Route index element={<Index></Index>}></Route>
+            <Route path="/login" element={<Login></Login>}></Route>
+            <Route path="/user" element={<UserPage></UserPage>}></Route>
+            <Route path="/admin" element={<Admin />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AppProvider>
   );
 };
 
