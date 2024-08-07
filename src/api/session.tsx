f6be1e62b3bab62e74f1ebe30854c5581a0c5fa8
Filippo Quattrocchi <filippo.quattrocchi23@outlook.it>
2024-08-07 14:40:53 +0900

    Feat: Add register new user

--- src/api/session.tsx
+++ src/api/session.tsx
@@ -2,6 +2,8 @@ import UserLoginType from "../interfaces/UserLoginType";
 import UserType from "../interfaces/UserType";
 
 const endPoint = import.meta.env.VITE_SERVER + "/sessions/";
+const endPointUser = import.meta.env.VITE_SERVER + "/users";
+
 
 export const checkSession = async () => {
   const response = await fetch(endPoint, {
@@ -28,6 +30,21 @@ export const login = async (userLogin: UserLoginType) => {
   } else {
     return null;
   }
+};
+
+export const register = async (userLogin: UserLoginType) => {
+	const configs: RequestInit = {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(userLogin),
+	};
+	const response = await fetch(endPointUser, configs);
+	if (response.status === 200) {
+		const user: UserType = await response.json();
+		return user;
+	} else {
+		return null;
+	}
 };
 
 export const logout = async () => {
